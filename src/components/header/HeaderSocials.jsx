02a880396869840e977c05f59a000e6e2c73b0a2
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-12-29 14:09:43 +0100

    fix header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
+import {SiDevpost} from 'react-icons/si'
 import {FaItchIo} from 'react-icons/fa'
 
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com/in/linusfackler" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/linusfackler" target="_blank"><BsGithub/></a>
-        <a href="https://linusfackler.itch.io/" target="_blank"><FaItchIo/></a>
+        <a href="https://linkedin.com/in/linusfackler" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/linusfackler" target="_blank" rel="noreferrer"><BsGithub/></a>
+        <a href="https://devpost.com/linusfackler" target="_blank" rel="noreferrer"><SiDevpost/></a>
+        <a href="https://linusfackler.itch.io/" target="_blank" rel="noreferrer"><FaItchIo/></a>
     </div>
   )
 }
